1fb399e683ff6077aabf083290ac4f9b983067d0
Davide Zeffiro <elrancid@gmail.com>
2017-10-04 13:44:51 +0200

    Leading zeros disallowed

--- ip-validator.js
+++ ip-validator.js
@@ -2,8 +2,9 @@
   'use strict';
 
   function validateInternetProtocol (input) {
-    return /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(input);
-  };
+    // leading zeros disallowed:
+    return /^(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$/.test(input);
+  }
 
   global.InternetProtocolValidator = {
     validate: validateInternetProtocol
